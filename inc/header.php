dc6262bfc4090c46c8c661632858c16b55feb626
tmanley1985 <tmanley1985@gmail.com>
2014-11-29 16:59:38 -0500

    Added header and footer files and skeleton

---
+++ inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Thomas Manley|Codeslinger</title>
+		<link rel="shortcut icon" href="/graphics/favicon.ico"/>
+		<link rel="stylesheet" type="text/css" href="/css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="/css/simplegrid.css">
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+		<script src="js/responsiveslides/responsiveslides.min.js"></script>			
+	</head>
+<body>
+	<div id="wrapper"> 
