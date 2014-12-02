b116c85f9edee6abc89aaa5a1207c2565c047f9b
tmanley1985 <tmanley1985@gmail.com>
2014-12-01 23:00:31 -0500

    Added web technologies and copyright info

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,24 @@
 	</div>
 	<footer>
+		<div class="grid grid-pad">
+			<div class="col-1-2 text-left">
+				<p>
+					
+					Web Technologies:<br>
+					PHP5, Object Oriented PHP, AJAX, JQUERY, JAVASCRIPT,<br>
+					SQL, HTML5, CSS3, GIT, SASS, FRONT END FRAMEWORKS,<br>
+					GIMP, MVC, APACHE.<br><br>
 		
+					Currently Learning:<br>
+					Ruby, Laravel, REST API Design, Spanish(¡Espero ser fluido en un año!).
+				
+				</p>
+			</div>
+		<div class="col-1-2 text-right">
+			<p class="copyright">&copy 2014 Thomas Manley</p>
+		</div>
+	</div>
+
 	</footer>
 </body>
 </html>
